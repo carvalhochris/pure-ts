22f73c8fb99ecdba53b1d7a441b8d5c2b874aafb
Christopher <kriss17388+dev@gmail.com>
2023-12-03 19:09:15 +0000

    rm stylesheets

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
 import ResourceList from "../components/ResourceList";
-import styles from "app/page.module.css";
 
 const ResourcesPage = () => {
   return (
     <>
-      <main className={styles.main}>
-        <h1>The Resources</h1>
-        <br />
-        <br />
-        <ResourceList />
-        <br />
-        <Link href="/">Home</Link>
-      </main>
+      <br />
+      <h1>The Resources</h1>
+      <br />
+      <br />
+      <ResourceList />
+      <br />
+      <Link href="/">Home</Link>
     </>
   );
 };
