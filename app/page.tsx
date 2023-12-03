dfa4115ac16133e61ee346c7932a2ad66f231961
Christopher <kriss17388+dev@gmail.com>
2023-12-03 17:21:53 +0000

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <h1>Welcome to Pure Typescript</h1>
+      <p>An online resouce for typescript developers and enthusiasts</p>
+      <p>coming soon...</p>
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
@@ -89,7 +92,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
-  )
+  );
 }
